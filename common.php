f93214a81824c93bbc82a6d7857fa4f9b634d784
Fred <62480197+ssl-origin@users.noreply.github.com>
2021-12-26 15:35:30 +0100

    v3.3.11

--- common.php
+++ common.php
@@ -17,6 +17,22 @@ if (empty($lang) || !is_array($lang))
 	$lang = array();
 }
 
+// NOTE À L'ATTENTION DES DÉVELOPPEURS
+//
+// Tous les fichiers de langue doivent utiliser l'encodage UTF-8 et les fichiers ne doivent pas contenir de BOM.
+//
+// Les caractères de remplacement peuvent désormais contenir des informations sur les commandes, par exemple au lieu de
+// 'Page %s de %s ", vous pouvez (et devez) écrire " Page %1$s de %2$s ", cela permet
+// aux traducteurs de réorganiser la sortie des données tout en s'assurant qu'elles restent correctes
+//
+// Vous n'en avez pas besoin lorsque des caractères de remplacement uniques sont utilisés, par exemple, "Message %d" est suffisant.
+// De même, lorsqu'une chaîne de caractères ne contient que deux caractères de remplacement qui sont utilisés pour encadrer un texte
+// dans une url, il n'est pas nécessaire de spécifier un ordre, par exemple "Cliquez sur %ICI%s" est suffisant
+//
+// Quelques caractères que vous pouvez copier/coller :
+// ’ » “ ” …
+//
+
 $lang = array_merge($lang, array(
 	'PLURAL_RULE'						=> 2,
 	
@@ -36,12 +52,12 @@ $lang = array_merge($lang, array(
 	'DIGESTS_DATE'						=> 'Date',
 	'DIGESTS_DELIMITER'					=> ' &#8249; ', // Utilisé pour faciliter l'affichage de la hiérarchie des noms de forum depuis l'index jusqu'en bas. Ne devrait pas changer dans les traductions linguistiques, sauf si un caractère différent est utilisé dans le fil d'Ariane.
 	'DIGESTS_DISABLED_MESSAGE'			=> 'Pour activer les champs, sélectionnez Réglages généraux et sélectionnez le type de résumé',
-	'DIGESTS_DISCLAIMER_HTML'			=> '<p><a href="%5$s">Désinscription en un clic</a></p><p>Vous pouvez également modifier ou supprimer votre abonnement du forum&rsquo;s <a href="%1$sucp.%3$s">Panneau de configuration utilisateur</a>.</p><p>Si vous avez des questions ou des commentaires sur vos résumés, veuillez les envoyer au <a href="mailto:%4$s?subject=Digests">%2$s webmaster</a>.</p>',
-	'DIGESTS_DISCLAIMER_TEXT'			=> 'Désinscription en un clic : %5$s
+		'DIGESTS_DISCLAIMER_HTML'			=> '<p><a href="%5$s">Désabonnement en un clic</a></p><p>Vous pouvez aussi modifier ou supprimer votre abonnement aux forums depuis le <a href="%1$sucp.%3$s">Panneau de contrôle de l&apos;utilisateur</a>.</p><p>Si vous avez des questions ou si vous souhaitez nous contacter veuillez adresser votre courriel au <a href="mailto:%4$s?subject=Digests">webmaster des forums de %2$s</a>.</p>',
+	'DIGESTS_DISCLAIMER_TEXT'			=> 'Désabonnement en un clic : %5$s
 	
-Vous pouvez également modifier ou supprimer votre abonnement à partir du panneau de contrôle de l‘utilisateur du forum.
+Vous pouvez aussi modifier ou supprimer votre abonnement aux forums depuis le Panneau de contrôle de l&apos;utilisateur.
 	
-Si vous avez des questions ou des commentaires sur vos résumés, veuillez les envoyer au webmaster : %4$s.',
+Si vous avez des questions ou si vous souhaitez nous contacter veuillez adresser votre courriel au webmaster des forums : %4$s.',
 	'DIGESTS_EXPLANATION'				=> 'Les résumés qui sont envoyés périodiquement sont constitués à partir des posts réalisés sur le forum. Les résumés peuvent être envoyés à une récurence journalière, hebdomadaire ou mensuelle et à une heure que vous choisisisez. Vous pouvez spécifier les forums dont les posts vous intéressent plus particulièrement, ou par défaut, vous pouvez sélectionner choisir de recevoir tous les posts de tous les forums. Vous pouvez annuler votre abonnement à tout moment en revenant sur cette page. Nombreux sont les utilisateurs qui trouvent ces résumés très utiles. Nous vous encourageons à faire un essai !',
 	'DIGESTS_FILTER_ERROR'				=> 'Le générateur de courriels a été appelé avec un user_digest_filter_type = %s invalide',
 	'DIGESTS_FILTER_FOES'				=> 'Retirer les posts émis par les Ignorés dans le résumé',
@@ -65,7 +81,7 @@ Si vous avez des questions ou des commentaires sur vos résumés, veuillez les e
 	'DIGESTS_FREQUENCY_EXPLAIN'			=> 'Les résumés hebdomadaires sont envoyés le %s. Les résumés mensuels sont envoyés le premier du mois. Le temps universel coordonné (UTC) est utilisé pour déterminer le jour de la semaine.',
 	'DIGESTS_FREQUENCY_SHORT'			=> 'Type de résumé',
 	'DIGESTS_HOURS_ABBREVIATION' 		=> ' h',	// cf. : http://www.scienceeditingexperts.com/which-is-the-correct-abbreviation-for-hours-2h-2-h-2hs-2-hs-2hrs-or-2-hrs, DIGESTS_AM and DIGESTS_PM sont sinon utilisés si spécifié dans user_dateformat
-	'DIGESTS_INSTALL_REQUIREMENTS'		=> 'Votre version de PHP doit être &gt ; 3.3.0 et &lt ; 4.0 pour installer cette extension. Veuillez résoudre ce problème, puis réessayer d‘activer l‘extension.',
+	'DIGESTS_INSTALL_REQUIREMENTS'		=> 'Votre version de PHP doit être &gt ; 3.3.0 et &lt ; 4.0 pour installer cette extension. Veuillez résoudre ce problème, puis réessayer d&apos;activer l&apos;extension.',
 	'DIGESTS_INTRODUCTION' 				=> 'Voici le résumé des derniers posts publiés sur les forums de %s. <em>Ne répondez pas</em> à ce courriel pour répondre aux sujets, posts ou messages privés. Mais <em>veuillez vous rendre sur le forum</em> pour rejoindre la discussion ! (En cas de problème d&apos;affichage du résumé, assurez-vous de bien avoir chargé tout le contenu distant)',
 	'DIGESTS_JUMP_TO_MSG'				=> 'Id Msg',
 	'DIGESTS_JUMP_TO_POST'				=> 'Id Post',
@@ -150,8 +166,8 @@ Si vous avez des questions ou des commentaires sur vos résumés, veuillez les e
 	'DIGESTS_TOC_EXPLAIN'				=> 'Si le forum est très actif, vous pouvez inclure un sommaire dans votre résumé. Dans les résumés stylés, le sommaire comporte des liens qui permettent d&apos;aller à un post en particulier ou à un message privé dans le résumé.',
 	'DIGESTS_UNKNOWN'					=> 'Inconnu',
 	'DIGESTS_UNREAD'					=> 'Non lu',
-	'DIGESTS_UNSUBSCRIBE_FAILURE'		=> 'Désolé, mais nous n‘avons pas pu vous désinscrire de la réception de résumés.',
-	'DIGESTS_UNSUBSCRIBE_SUCCESS'		=> 'Vous avez été désabonné de la réception d‘autres résumés.',
+	'DIGESTS_UNSUBSCRIBE_FAILURE'		=> 'Désolé, mais nous n&apos;avons pas pu vous désabonner de la liste d&apos;envoi des résumés.',
+	'DIGESTS_UNSUBSCRIBE_SUCCESS'		=> 'Vous avez été désabonné de la liste d&apos;envoi des résumés.',
 	'DIGESTS_UPDATED'					=> 'Les réglages de votre résumé ont été enregistrés',
 	'DIGESTS_USE_BOOKMARKS'				=> 'Les sujets cochés seulement',
 	'DIGESTS_WEEKDAY' 					=> 'Dimanche,Lundi,Mardi,Mercredi,Jeudi,Vendredi,Samedi',
